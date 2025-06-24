1ca82819f4bbf47c8dcbc4a85791c2e62d0f6224
JiaminLo <jluo6565@conestogac.on.ca>
2025-06-24 00:11:13 -0400

    add css class for page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,16 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>
-
-        Recreation Time Limiter
+         WatchDog
         </h1>
         <input type="number" placeholder="Enter minutes"
         value={minutes}
         onChange={handleSave}
+        required
+        className="border valid:border-green-500 focus:outline-none pl-1 py-2 rounded-sm" 
         />
+        <button className="bg-sky-500 hover:bg-sky-700  cursor-pointer rounded-sm p-2 ">Save changes</button>
+
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       @copyright Jiamin Luo 2025  
