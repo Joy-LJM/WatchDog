050449b4b121d9784f5cfded6c3a5d77fdc63f45
JiaminLo <jluo6565@conestogac.on.ca>
2025-08-27 18:49:02 -0400

    chore: add eslint prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    extends: ["plugin:prettier/recommended"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
